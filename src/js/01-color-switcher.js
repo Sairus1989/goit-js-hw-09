79cdde935a0b03a35d92780230f912a785512241
Сергей <sbukij@gmail.com>
2022-06-05 12:58:30 +0300

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+startBtn.addEventListener('click', onColorSwitcher);
+stopBtn.addEventListener('click', offColorSwitcher);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  function createBodyColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+  };
+
+  function onColorSwitcher() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval (createBodyColor, 1000);
+  };
+
+  function offColorSwitcher() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId); 
+  }
